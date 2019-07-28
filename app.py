33e7830f4e7971112bfca8154b160f922612975d
AlabdulhadiSara <alabdulhadis@hotmail.com>
2019-07-28 23:09:50 +0300

    Sync

--- app.py
+++ app.py
@@ -1,26 +1,125 @@
-from flask import Flask
-from flask import render_template
 from pymongo import MongoClient
 import json
 from bson import json_util
 from bson.json_util import dumps
-import dns # required for connecting with SRV
+import dns 
 import csv
 from pathlib import Path
+import os
+import numpy as np
+import pickle
+from sklearn.externals import joblib
+from sklearn.preprocessing import PolynomialFeatures 
+from flask import Flask, render_template, url_for, request, session, redirect, jsonify, flash
+from flask_pymongo import PyMongo
+import bcrypt
+from wtforms import Form, StringField, PasswordField, validators, SubmitField, SelectField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 
 app = Flask(__name__)
 
-URI_string = "mongodb+srv://explorer:1234567890@datacenter-nfv4t.gcp.mongodb.net/test?retryWrites=true&w=majority"
+URI_string = "mongodb+srv://Sarah:admin@cluster0-s9og8.gcp.mongodb.net/test?retryWrites=true&w=majority"
+
+class SignupForm(Form):
+    
+    Username = StringField('Username', [
+        validators.DataRequired(message=('Please enter a username'))
+    ])
+    
+    Email = StringField('Email', [
+        validators.DataRequired(),
+        validators.Email(message=('Please enter a vaild email address')),
+        EqualTo('Email1', message=('Emails must match'))
+    ])
+    
+    Email1 = StringField('Repeat Email',validators=[DataRequired()])
+    
+    Password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.Length(min=8, message=('Password should have 8 characters at leats')),
+        EqualTo('Password1', message=('Passwords must match'))
+    ])
+    
+    Password1 = PasswordField('Repeat Password',validators=[DataRequired()])
+
+class LoginForm(Form):
+    Username = StringField('Username', validators=[DataRequired()])
+    Password = PasswordField('Password', validators=[DataRequired()])
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if 'Username' in session:
+        return render_template("index.html")
+    return render_template("login.html")
 
+@app.route("/statistics")
+def statistics():
+    if 'Username' in session:
+        return render_template("Statistics.html")
+    return render_template("login.html")
+
+@app.route('/eligibility')
+def eligibility():
+    if 'Username' in session:
+        return render_template('eligibility.html')
+    return render_template("login.html")
+
+@app.route('/InterestRate')
+def interestRate():
+    if 'Username' in session:
+        return render_template('InterestRate.html')
+    return render_template("login.html")
+
+@app.route('/addLoan')
+def addLoan():
+    if 'Username' in session:
+        return render_template('addLoan.html')
+    return render_template("login.html")
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    
+    login_form = LoginForm(request.form) 
+    if request.method == 'POST' and login_form.validate():
+        connection = MongoClient(URI_string)
+        users = connection["SummerTraining"]["Users"]
+        login_user = users.find_one({'Username' : request.form['Username']})
+        
+        if login_user and bcrypt.hashpw(request.form['Password'].encode('utf-8'), login_user['Password']) == login_user['Password']:
+            session['Username'] = request.form['Username']
+            return redirect(url_for('index'))
+
+        flash('Invalid username/password combination')
+        return render_template('login.html', form =login_form)
+    return render_template('login.html', form =login_form)
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    signup_form = SignupForm(request.form) 
+    if request.method == 'POST' and signup_form.validate():
+        connection = MongoClient(URI_string)
+        users = connection["SummerTraining"]["Users"]
+        existing_user = users.find_one({'Username' : request.form['Username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['Password'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'Username' : request.form['Username'], 'Email' : request.form['Email'], 'Password' : hashpass})
+            session['Username'] = request.form['Username']
+            return redirect(url_for('index'))
+
+        flash('Username already exists!')
+        return render_template('register.html', form =signup_form)
+    return render_template('register.html', form =signup_form)
+
+@app.route('/sign_out')
+def sign_out():
+    session.pop('Username')
+    return redirect(url_for('index'))
 
 @app.route("/Loans/SummerTraining")
-def donorschoose_projects():
+def connectCloudDatabases():
     connection = MongoClient(URI_string)
-    collection = connection["loans_data_base"]["data"]
+    collection = connection["SummerTraining"]["Loans"]
     projects = collection.find( {}, {"_id" : False, "funded_amnt" : True, "int_rate" : True, "emp_length" : True,
         "annual_inc" : True,"loan_status" : True, "num_sats" : True, "last_pymnt_amnt" : True,
         "avg_cur_bal" : True, "addr_state" : True }) 
@@ -28,8 +127,73 @@ def donorschoose_projects():
     for project in projects:
         json_projects.append(project)
     json_projects = json.dumps(json_projects, default=json_util.default)
-    connection.close()
     return json_projects
 
-if __name__ == "__main__":
+@app.route('/eligibility', methods=['POST'])
+def predictEligibility():
+    funded_amnt = request.form['funded_amnt']
+    emp_length = request.form['emp_length']
+    avg_cur_bal = request.form['avg_cur_bal']
+    num_sats = request.form['num_sats']
+    num_actv_rev_tl = request.form['num_actv_rev_tl']
+    installment = request.form['installment']
+    bc_util = request.form['bc_util']
+    total_acc = request.form['total_acc']
+    to_predict_list = [funded_amnt, emp_length, avg_cur_bal, num_actv_rev_tl,  num_sats, installment, bc_util, total_acc]
+    with open('RFM.pkl','rb') as f:
+        loaded_model =  pickle.load(f)
+    result = loaded_model.predict([to_predict_list]) 
+    if result[0] == 1:
+        return jsonify({'output': 'accepted!'})
+    else: 
+        return jsonify({'output3': 'rejected!'})
+
+@app.route('/InterestRate', methods=['POST'])
+def predictInterestRate():
+    funded_amnt = request.form['funded_amnt']
+    emp_length = request.form['emp_length']
+    annual_inc = request.form['annual_inc']
+
+    to_predict_list = [[funded_amnt, emp_length, annual_inc]]
+    polynomial_features = PolynomialFeatures(degree=3)
+    to_predict_list = polynomial_features.fit_transform(to_predict_list)
+    with open('IntRateRegression.pkl','rb') as f:
+        loaded_model =  pickle.load(f)
+    result = loaded_model.predict(to_predict_list)    
+    return jsonify({'output': str(float("{0:.2f}".format(result[0]))) + '%'})
+
+@app.route('/addLoan', methods=['POST'])
+def predictEligibilityAndInterestRate():
+    funded_amnt = request.form['funded_amnt']
+    emp_length = request.form['emp_length']
+    avg_cur_bal = request.form['avg_cur_bal']
+    num_sats = request.form['num_sats']
+    num_actv_rev_tl = request.form['num_actv_rev_tl']
+    installment = request.form['installment']
+    bc_util = request.form['bc_util']
+    total_acc = request.form['total_acc']
+    annual_inc = request.form['annual_inc']
+
+    to_predict_list1 = [funded_amnt, emp_length, avg_cur_bal, num_actv_rev_tl,  num_sats, installment, bc_util, total_acc]
+    to_predict_list2 = [[funded_amnt, emp_length, annual_inc]]
+    polynomial_features = PolynomialFeatures(degree=3)
+    to_predict_list2 = polynomial_features.fit_transform(to_predict_list2)
+    with open('RFM.pkl','rb') as f1:
+        loaded_model1 =  pickle.load(f1)
+    with open('IntRateRegression.pkl','rb') as f2:
+        loaded_model2 =  pickle.load(f2)
+    result1 = loaded_model1.predict([to_predict_list1])
+    result2 = loaded_model2.predict(to_predict_list2) 
+    
+    if result1[0] == 1:
+        return jsonify({
+            'output': 'accepted!',
+            'output1': 'The interest rate should be around ',
+            'output2': str(float("{0:.2f}".format(result2[0]))) + '%'})
+    else: 
+        return jsonify({'output3': 'rejected!'})
+
+    
+if __name__ == '__main__':
+    app.secret_key = 'mysecret'
     app.run(host='0.0.0.0',port=5000,debug=True)
---
+++ static/js/charts.js
@@ -0,0 +1,272 @@
+queue()
+    .defer(d3.json, "/Loans/SummerTraining")
+    .await(makeGraphs);
+
+function makeGraphs(error, LoansJson) {
+    
+    var ndx = crossfilter(LoansJson);
+
+    var loanStatus = ndx.dimension(function(d) { return d["loan_status"]; });
+    var paid = ndx.dimension(function(d) { return d.loan_status === "Fully Paid" ? 'Loss' : 'Gain';})
+    var intRate = ndx.dimension(function(d) { return d["int_rate"]; });
+    var fundedAmnt = ndx.dimension(function(d) { return d["funded_amnt"]; });
+    var empLength = ndx.dimension(function(d) { return d["emp_length"]; });
+    var annualInc  = ndx.dimension(function(d) { return d["annual_inc"]; });
+    var numSats = ndx.dimension(function(d) { return d["num_sats"]; });
+    var lastPymntAmnt = ndx.dimension(function(d) { return d["last_pymnt_amnt"]; });
+    var avgCurBal = ndx.dimension(function(d) { return d["avg_cur_bal"]; });
+    var addrState  = ndx.dimension(function(d) { return d["addr_state"]; });
+    
+    var all = ndx.groupAll();
+    var numLoansByStatus = loanStatus.group(); 
+    var numLoansByEmpLength = empLength.group();
+    var numLoansByNumSats = numSats.group();
+    var totalFundeedAmnt = ndx.groupAll().reduceSum(function(d) {return d["funded_amnt"];});
+    var totalInterestRate = ndx.groupAll().reduceSum(function(d) {return d["int_rate"];});
+    var countInterestRate = ndx.groupAll().reduceCount(function(d) {return d["int_rate"];});
+    var avgInterestRate = totalInterestRate / countInterestRate;
+    var loansVolumeByStates = addrState.group();
+    var countLoans = numLoansByStatus.reduceCount().all();
+    var counts = loansVolumeByStates.reduceCount().all();
+
+    var meaninterestRate = ndx.groupAll().reduce(
+        function (p, v) {
+            ++p.n;
+            p.tot += v.int_rate;
+            return p;
+        },
+        function (p, v) {
+            --p.n;
+            p.tot -= v.int_rate;
+            return p;
+        },
+        function () { return {n:0,tot:0}; }
+    );
+    var average = function(d) {
+        return d.n ? (d.tot / d.n) : 0;
+    };
+
+    var countByState = {}; 
+        
+    Array.prototype.slice.call(counts).forEach(function(d) { countByState[d.key] = d.value; })
+    
+    var loansVolumeByStates = loansVolumeByStates.reduceSum(function(d, i) { 
+        return d.int_rate / countByState[d.addr_state]; 
+    });
+
+    var empLengthD = ndx.dimension(function(d) {
+        return d.emp_length;
+    });
+
+    var empLengthByStatus = empLengthD.group().reduce( 
+        function(p, v) {
+            if (isFullyPaid(v)) {
+                p.totalFullyPaidE++;
+            }
+            if (isChargedOff(v)) {
+                p.totalChargedOffE++;
+            }
+            return p;
+        },
+        function(p, v) {
+            if (isFullyPaid(v)) {
+                p.totalFullyPaidE--;
+            }
+            if (isChargedOff(v)) {
+                p.totalChargedOffE--;
+            }
+            return p;
+        },
+        function() {
+            return {
+                totalFullyPaidE:0,
+                totalChargedOffE:0,
+            };
+        }
+);
+
+    function isFullyPaid(v) {
+        return v.loan_status === "Fully Paid" ;
+    }
+    function isChargedOff(v) {
+        return v.loan_status === "Charged Off" ;
+    }
+    var sats = ndx.dimension(function(d) {
+        return d.num_sats;
+    });
+    
+
+    var crimeIncidentByYear = sats.group().reduce( 
+            function(p, v) {
+                if (isFullyPaid(v)) {
+                    p.totalFullyPaid++;
+                }
+                if (isChargedOff(v)) {
+                    p.totalChargedOff++;
+                }
+                return p;
+            },
+            function(p, v) {
+                if (isFullyPaid(v)) {
+                    p.totalFullyPaid--;
+                }
+                if (isChargedOff(v)) {
+                    p.totalChargedOff--;
+                }
+                return p;
+            },
+            function() {
+                return {
+                    totalFullyPaid:0,
+                    totalChargedOff:0,
+                };
+            }
+    );
+       
+    
+    closeGroup      = empLengthD.group().reduce(
+        function(p,v) {
+          p.splice(d3.bisectLeft(p, v.close), 0, v.close);
+          return p;
+        },
+        function(p,v) {
+          p.splice(d3.bisectLeft(p, v.close), 1);
+          return p;
+        },
+        function() {
+          return [];
+        }
+      );
+    
+    var chart = dc.barChart("#box-test");
+          
+    chart
+    .width(768)
+    .height(480)
+    .margins({top: 10, right: 50, bottom: 30, left: 50})
+    .dimension(empLengthD)
+    .group(closeGroup)
+    .x(d3.scale.linear().domain([0,11]))
+    .round(d3.timeMonth.round)
+    .elasticY(true);
+    
+    
+    var incidentChart = dc.barChart('#incident-chart');
+    var boxChartEmpLength = dc.barChart('#empLength-chart');
+    var loanStatusChart = dc.rowChart('#resource-type-row-chart');
+    var usChart = dc.geoChoroplethChart('#us-chart');
+    var numberOfLoans = dc.numberDisplay('#number-of-loans');
+    var totalFundeedAmntND = dc.numberDisplay('#total-funded-amnt');
+    var totalInterestRateND = dc.numberDisplay('#total-int-rate');
+
+    numberOfLoans
+        .formatNumber(d3.format(",d"))
+        .valueAccessor(function(d){return d; })
+        .group(all);
+
+    totalFundeedAmntND
+        .formatNumber(d3.format(",d"))
+        .valueAccessor(function(d){return d; })
+        .group(totalFundeedAmnt)
+        .formatNumber(d3.format(".5s"));
+
+    totalInterestRateND
+        .group(meaninterestRate)
+        .valueAccessor(average)
+        .formatNumber(d3.format(".3g"));
+
+    loanStatusChart
+        .width(300)
+        .height(250)
+        .dimension(loanStatus)
+        .group(numLoansByStatus)
+        .xAxis().ticks(4);
+/*
+    empLengthChart
+        .width(300)
+        .height(250)
+        .dimension(empLength)
+        .group(numLoansByEmpLength)
+        .xAxis().ticks(4);
+*/
+    usChart
+        .width(1000)
+        .height(500)
+        .dimension(addrState)
+        .group(loansVolumeByStates)
+        .colors(["#E2F2FF", "#C4E4FF", "#9ED2FF", "#6BBAFF",  "#36A2FF"])
+        .colorDomain([11, 16])
+        .overlayGeoJson(statesJson["features"], "state", function (d) {
+            return d.properties.name;
+        })
+        .projection(d3.geo.albersUsa()
+                    .scale(900)
+                    .translate([400, 200]))
+        .title(function (p) {
+            return "State: " + p["key"]
+                    + "\n"
+                    + "Average Interest Rate: " + p["value"] + " %";
+        })
+
+    usChart.legendables = function () {
+        var range = usChart.colors().range()
+        var domain = usChart.colorDomain()
+        var step = 1
+        var val = domain[0] 
+            return range.map(function (d, i) {
+                var legendable = {name: val + ' - ' + (val+step) + " %", chart: usChart};
+                legendable.color = usChart.colorCalculator()(val);
+                val += step
+                return legendable;
+            });
+        }; 
+            
+    usChart.legend(
+        dc.legend()
+            .x(800)
+            .y(70)
+            .itemHeight(500/30)           
+    );
+    
+    incidentChart
+        .width(550)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 40, left: 40})
+        .dimension(sats)
+        .colors(["#E2F2FF", "#36A2FF"])
+        .group(crimeIncidentByYear, "Charged Off")
+        .valueAccessor(function(d) {
+            return d.value.totalChargedOff;
+        })
+        .stack(crimeIncidentByYear, "Fully Paid", function(d){return d.value.totalFullyPaid;})
+        .x(d3.scale.linear().domain([0,70]))
+        .renderHorizontalGridLines(true)
+        .centerBar(true)
+        .elasticY(true)
+        .legend(dc.legend().x(400).y(0))
+        .xAxisLabel("Number of Satisfactory Accounts")
+        .xAxis().ticks(6).tickFormat(d3.format("d"));
+
+    
+    boxChartEmpLength
+        .width(550)
+        .height(300)
+        .margins({top: 40, right: 50, bottom: 40, left: 40})
+        .dimension(empLengthD)
+        .colors(["#E2F2FF", "#36A2FF"])
+        .group(empLengthByStatus, "Charged Off")
+        .valueAccessor(function(d) {
+            return d.value.totalChargedOffE;
+        })
+        .stack(empLengthByStatus, "Fully Paid", function(d){return d.value.totalFullyPaidE;})
+        .x(d3.scale.linear().domain([0,11]))
+        .renderHorizontalGridLines(true)
+        .centerBar(true)
+        .elasticY(true)
+        .legend(dc.legend().x(400).y(0))
+        .xAxisLabel("Employment Length")
+        .xAxis().ticks(6).tickFormat(d3.format("d"));
+
+    dc.renderAll();
+
+};
