17adb6b6515060c7fdeddd7ea3a7c758bbbdfce4
ash <32853027+asaah18@users.noreply.github.com>
2019-07-30 10:52:32 +0300

    prepare for deployment

--- main.py
+++ main.py
@@ -194,6 +194,6 @@ def predictEligibilityAndInterestRate():
         return jsonify({'output3': 'rejected!'})
 
     
-if __name__ == '__main__':
-    app.secret_key = 'mysecret'
-    app.run(host='0.0.0.0',port=5000,debug=True)
+# if __name__ == '__main__':
+#     app.secret_key = 'mysecret'
+#     app.run(host='0.0.0.0',port=5000,debug=True)
