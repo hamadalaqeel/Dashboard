36d97f2dc94631e1ccde05d6fd39c5fb1ca1a6b6
ash <32853027+asaah18@users.noreply.github.com>
2019-07-31 22:04:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -196,4 +196,4 @@ def predictEligibilityAndInterestRate():
     
 if __name__ == '__main__':
     app.secret_key = 'mysecret'
-    app.run(host="127.0.0.1",port=8080)
+    app.run(host="127.0.0.1",port=8080,debug=True)
